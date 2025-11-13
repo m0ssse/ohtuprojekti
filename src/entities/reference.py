2d6c3664caef4d57decc652fd665ecaaa66d4497
Mullil <eetu.taatila@gmail.com>
2025-11-13 21:26:49 +0200

    add database methods

---
+++ src/entities/reference.py
@@ -0,0 +1,21 @@
+class Reference:
+    def __init__(self, id,
+        ref_type, author, title,
+        year, booktitle=None, publisher=None, 
+        journal=None, pages=None, volume=None,
+        edition=None, doi=None, chapter=None, address=None):
+
+        self.id = id
+        self.ref_type = ref_type
+        self.author = author
+        self.title = title
+        self.year = year
+        self.booktitle = booktitle
+        self.publisher = publisher
+        self.journal = journal 
+        self.pages = pages
+        self.volume = volume
+        self.edition = edition
+        self.doi = doi
+        self.chapter = chapter
+        self.address = address
