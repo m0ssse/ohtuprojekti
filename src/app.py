9aecfe3c80f9a6b5ac845b5d6dbaf910bc07e3db
Antti Moisio <moisioa1@gmail.com>
2025-11-14 12:24:32 +0200

    Added first html templates

--- src/app.py
+++ src/app.py
@@ -1,14 +1,11 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
-from repositories.todo_repository import get_todos, create_todo, set_done
 from config import app, test_env
 from util import validate_todo
 
 @app.route("/")
 def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished) 
+    return render_template("index.html") 
 
 @app.route("/new_todo")
 def new():
