43c85d2a83d93224b122949b30c62511edd2ca75
Mullil <eetu.taatila@gmail.com>
2025-11-17 20:03:14 +0200

    add tests for references

---
+++ src/tests/reference_repository_test.py
@@ -0,0 +1,42 @@
+import unittest
+from entities.reference import Reference
+from db_helper import reset_db
+from repositories.reference_repository import create_reference, delete_reference, get_references
+from app import app
+
+class TestReferenceMethods(unittest.TestCase):
+    def setUp(self):
+        self.app_context = app.app_context()
+        self.app_context.push()
+        reset_db()
+
+    def test_valid_reference_is_created(self):
+        ref = Reference(id=1, ref_type="book", author="test", title="title", year=2003)
+        create_reference(ref)
+        references = get_references()
+        self.assertEqual(len(references), 1)
+        self.assertEqual(type(references[0]), type(ref))
+        self.assertEqual(references[0].id, ref.id)
+        self.assertEqual(references[0].author, ref.author)
+        self.assertEqual(references[0].title, ref.title)
+        self.assertEqual(references[0].year, ref.year)
+
+
+    def test_get_references_returns_correct_amout(self):
+        ref1 = Reference(id=1, ref_type="book", author="test", title="title", year=2003)
+        create_reference(ref1)
+        ref2 = Reference(id=2, ref_type="book", author="test2", title="title2", year=2004)
+        create_reference(ref2)
+        references = get_references()
+        self.assertEqual(type(references), list)
+        self.assertEqual(len(references), 2)
+
+    def test_delete_reference_removes_references(self):
+        ref1 = Reference(id=1, ref_type="book", author="test", title="title", year=2003)
+        create_reference(ref1)
+        ref2 = Reference(id=2, ref_type="book", author="test2", title="title2", year=2004)
+        create_reference(ref2)
+        delete_reference(ref_id=1)
+        references = get_references()
+        self.assertEqual(len(references), 1)
+        self.assertEqual(references[0].id, ref2.id)
