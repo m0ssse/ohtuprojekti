de63564913765630685033b49b11d1e732b17f52
Antti Moisio <moisioa1@gmail.com>
2025-11-25 13:18:46 +0200

    Fixed required field check in backend

--- src/util.py
+++ src/util.py
@@ -8,7 +8,7 @@ def validate_reference(form_contents) -> tuple:
     form_ok = True
     ref_type = form_contents["ref_type"]
     for field in REQUIRED_FIELDS[ref_type]:
-        if field not in form_contents:
+        if field not in form_contents or not form_contents[field]:
             form_ok = False
             missing.append(field)
     return form_ok, missing
