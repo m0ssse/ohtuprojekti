6d85a266ee7fc10787b20339e15bdb7a5285d207
Mullil <eetu.taatila@gmail.com>
2025-11-26 08:55:55 +0200

    add error checks for delete and get_one_reference

--- src/tests/reference_repository_test.py
+++ src/tests/reference_repository_test.py
@@ -1,7 +1,11 @@
 import unittest
 from entities.reference import Reference
 from db_helper import reset_db
-from repositories.reference_repository import create_reference, delete_reference, get_references, get_one_reference
+from repositories.reference_repository import (
+    create_reference, delete_reference,
+    get_references, get_one_reference,
+    DeleteFailureError, SelectFailureError
+)
 from app import app
 
 class TestReferenceMethods(unittest.TestCase):
@@ -52,3 +56,19 @@ class TestReferenceMethods(unittest.TestCase):
         self.assertEqual(reference.author, ref2.author)
         self.assertEqual(reference.title, ref2.title)
         self.assertEqual(reference.year, ref2.year)
+
+    def test_invalid_delete_fails_and_raises_exception(self):
+        ref1 = Reference(id=1, ref_type="book", author="test", title="title", year=2003)
+        create_reference(ref1)
+
+        with self.assertRaises(DeleteFailureError):
+            delete_reference(ref_id=111112111114313209876)
+        references = get_references()
+        self.assertEqual(len(references), 1)
+
+    def test_invalid_get_reference_raises_exception(self):
+        ref1 = Reference(id=1, ref_type="book", author="test", title="title", year=2003)
+        create_reference(ref1)
+
+        with self.assertRaises(SelectFailureError):
+            get_one_reference(ref_id=111112111114313209876)
